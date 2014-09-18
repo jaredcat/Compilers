9ea4a4f865ea445242babfcca610895f8810dae5
Jared Kotoff <krylic@gmail.com>
2014-09-18 12:53:40 -0700

    Project 2 Part 1

--- Project 2/Project2-1.py
+++ Project 2/Project2-1.py
@@ -7,22 +7,32 @@
 #Given a word determines if it is accepted by the language
 def language(w):
     wlist = list(w)
+    #if the first letter is "a"
     if wlist[0] == "a":
+        #loop though all letters in input
         for i, j in enumerate(wlist):
+            #lets a* loop.
             if j != "a":
                 if i+1 != len(wlist):
                     return "rejected"
-                else:
+                elif j == "b":
                     return "accepted"
         return "rejected"
+    #if the first letter is "b"
     elif wlist[0] == "b":
         for i in wlist:
+            #the word can only contain "b"
             if i != "b":
                 return "rejected"
+        #the word is composed of only 'B's and is accepted
         return "accepted"
+    #no valid input found
+    return "rejected"
+
 
 
 while 1 == 1:
     #get user inputted word
     word = input("Enter word: ")
-    print("The word is "+language(word))s
+    #prints out if the word is accepted or not
+    print("The word is "+language(word))
