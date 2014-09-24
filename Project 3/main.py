ffce615c35fe6b3bb28735c30beded2a9eb9d827
Jared Kotoff <krylic@gmail.com>
2014-09-23 17:04:50 -0700

    Project 3

--- Project 3/main.py
+++ Project 3/main.py
@@ -1,21 +1,32 @@
 ##Pooja Srivastava & Jared Kotoff
 #CS 323
-#Programming Assignment 2
+#Programming Assignment 3
 #September 25, 2014
 
 #!/usr/bin/python
 import re
+import string
 
 # Open this file.
-f = open("testFile.txt", "r")
+f = open("data.txt", "r")
+g = open("newdata.txt", "w")
+tlist = []
+
+#Alpa holds a set of letters
+alpha = set(string.ascii_letters)
+
+#Num holds a set of digits
+num = set(string.digits)
+
+symb = set(string.punctuation)
 
 # Loop over each line in the file
 for line in f.readlines():
 
-    if not line.startswith("//"):
-    # Strip the line to remove whitespace.
-        print(line.strip(' \t\n\r'))
-
+    if not line.startswith("//") and not line.startswith("\n"):
+        # Strip the line to remove whitespace.
+        line = line.strip(' \t\r')
+        tlist = line.split(" ")
     # Split the line.
     line = line.split(";")
 
