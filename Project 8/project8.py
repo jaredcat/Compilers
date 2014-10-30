9ae48c20dbb3546576418ece3edc0a7d354f7df0
Jared Kotoff <krylic@gmail.com>
2014-10-29 20:16:47 -0700

    Finished project 8. Added comments.

--- Project 8/project8.py
+++ Project 8/project8.py
@@ -3,17 +3,12 @@
 #Programming Assignment 6
 #October 9, 2014
 
-#Tokens separated by class
-classify = ([
-    ["type", "int", "float"],
-    ["reserved word", "main", "cout<<"],
-    ["operator", "+", "-", "*", "/", "="],
-    ["separator", "(", ")", ";", "{", "}", ")", ","]
-])
 
+#Potential terminal tokens for this language
 tokens = ['a', '+', '-', '*', '/', '(', ')', '$']
 
 
+#Predictive parsing table
 def table(nonterminal, terminal):
     if nonterminal == 'E':
         if terminal == 'a' or terminal == '(':
@@ -44,39 +39,59 @@ def table(nonterminal, terminal):
         return ''
 
 
+#Function to print the current stack to terminal
+def print_stack(stack):
+    print('Stack: ', end='')
+    for i in stack:
+        print(i, end='')
+    print()
+
+
+#Traces the input
 def check_input(line):
     i = 0
     stack = ([])
     stack.append('$')
     stack.append('E')
-    while stack:
-        print(stack)
+    print_stack(stack)
 
+    while stack:
+        #if line ends before the stack is empty then reject
+        if (len(stack) != 0 and len(line) - i == 0):
+            return 'Rejected'
+
+        #Looks at the last item in the stack
         token = stack[len(stack)-1]
+        #First item in the input
         read = line[i]
 
+        #If the token is terminal
         if token in tokens:
+            #if the token is the same as the one from the input
             if token == read:
                 stack.pop()
+                print_stack(stack)
                 i += 1
             else:
-                return "Rejected"
-        else:
+                #terminal token but not the same
+                return 'Rejected'
+        else: #Token is non-terminal
             lookup = table(token, read)
-            if lookup == '%':
+            if lookup == '%': #if lamda
                 stack.pop()
-            elif lookup != '':
+            elif lookup: #if not empty
                 stack.pop()
                 for j in reversed(lookup):
                     stack.append(j)
             else:
-                return "Rejected"
+                return 'Rejected'
     return 'Accepted'
 
 
 def main():
     line = input()
     print(check_input(line))
+
 
 if __name__ == '__main__':
     main()
