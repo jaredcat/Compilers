86301809adec74dee51c3c6d8dbd56fbe3db334c
Jared Kotoff <krylic@gmail.com>
2014-12-02 01:00:53 -0800

    Started final project

---
+++ Project 10 (Final)/final.py
@@ -0,0 +1,22 @@
+#Pooja Srivastava & Jared Kotoff
+#CS 323
+#Final Project
+#December 11, 2014
+
+
+def postfix(s):
+    postfixed[] = 0
+    return postfixed
+
+
+def readFile():
+
+
+def main():
+    #Part 1
+        #Call readFile and read in finalv1.txt
+        #Call postfix and create finalv2.txt
+    #Part 2
+        #
+
+    return 0
